7c781c0a1b4cae5a49b8b4f5878d06ad1fdd66a3
ElliotPi <103081209+ElliotPi@users.noreply.github.com>
2024-01-17 23:34:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        int NumOfRolls = int.Parse(Console.ReadLine());
+
+        RollDice.RollDice rs = new();
+
+        int[] results = rs.RollingSimulator(NumOfRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {NumOfRolls}.\n");
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            int percentage = (results[i] * 100 / NumOfRolls);
+            string totalAsterisks = "";
+            for (int count = 0; count <= percentage; count++)
+            {
+                totalAsterisks += "*";
+            }
+            Console.WriteLine($"{i + 2}: {totalAsterisks}");
+
+        }
+
+
+    }
+}
+
---
+++ RollDice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollDice
+{
+    internal class RollDice
+    {
+        private int Rolls;
+
+
+        // Constructor
+        public RollDice()
+        {
+            Rolls = 0;
+        }
+       
+        public int[] RollingSimulator(int Rolls)
+        {
+            // the array stores the total num of results for 2-12, in total 11 numbers
+            // array[0] = the num of 2s, array[1] = the num of 3s, array[2] = the num of 4s and so on
+            int[] results = new int[11];
+            Random random = new Random();
+
+            for (int count = 0; count <= Rolls; count++)
+            {
+
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+                int result = dice1 + dice2;
+
+                // since the results will be between 2-12, to locate the correct index for the corresponding result will be "result - 2" (aka 0-10)
+                results[(result - 2)]++;
+            }
+
+            return results;
+        
+        }
+        
+    }
+}
